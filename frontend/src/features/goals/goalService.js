3cdfab83ee12c5cd85e56e49ce5c4a1c4e95ed4c
Jaret Balba <Jaretzbalba@gmail.com>
2023-01-20 13:31:04 -0800

    change API_URL to hosted backend

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = '/api/goals/'
+const API_URL = 'https://mygoals-backend.onrender.com/api/goals/';
 
 // Create new goal
 const createGoal = async (goalData, token) => {
@@ -8,25 +8,25 @@ const createGoal = async (goalData, token) => {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  }
+  };
 
-  const response = await axios.post(API_URL, goalData, config)
+  const response = await axios.post(API_URL, goalData, config);
 
-  return response.data
-}
+  return response.data;
+};
 
 // Get user goals
-const getGoals = async (token) => {
+const getGoals = async token => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  }
+  };
 
-  const response = await axios.get(API_URL, config)
+  const response = await axios.get(API_URL, config);
 
-  return response.data
-}
+  return response.data;
+};
 
 // Delete user goal
 const deleteGoal = async (goalId, token) => {
@@ -34,17 +34,17 @@ const deleteGoal = async (goalId, token) => {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  }
+  };
 
-  const response = await axios.delete(API_URL + goalId, config)
+  const response = await axios.delete(API_URL + goalId, config);
 
-  return response.data
-}
+  return response.data;
+};
 
 const goalService = {
   createGoal,
   getGoals,
   deleteGoal,
-}
+};
 
-export default goalService
+export default goalService;
