314890acd6529b489af473cd61dfe9455a5dff8f
Jaret Balba <Jaretzbalba@gmail.com>
2023-01-20 11:50:28 -0800

    intitial setup

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 2000;
 
 connectDB();
 
@@ -21,9 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
